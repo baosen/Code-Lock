24f8dfbb3d2a3f48e6af6db9628683c62304f595
bao <chibaosen@gmail.com>
2018-03-22 21:20:32 +0100

    More pseudocode.

--- app/src/main/java/baosen/codelock/CodeLockTelephonyConnectionService.java
+++ app/src/main/java/baosen/codelock/CodeLockTelephonyConnectionService.java
@@ -6,11 +6,19 @@ import android.telecom.Connection;
 import android.telecom.ConnectionRequest;
 import android.telecom.ConnectionService;
 import android.telecom.PhoneAccountHandle;
+import android.telephony.PhoneNumberUtils;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class CodeLockTelephonyConnectionService extends ConnectionService { // TODO: Inherit from TelephonyConnectionService.
     @Override
     public Connection onCreateOutgoingConnection(PhoneAccountHandle connectionManagerPhoneAccount, final ConnectionRequest request) {
-        return super.onCreateOutgoingConnection(connectionManagerPhoneAccount, request);
+        String numberToDial = request.getAddress().getSchemeSpecificPart();
+
+        // Check if the user is dialing an emergency number.
+        if (PhoneNumberUtils.isLocalEmergencyNumber(this, numberToDial))
+            // Do the call!
+            return super.onCreateOutgoingConnection(connectionManagerPhoneAccount, request);
+
+        // TODO: Ask for code.
     }
 }

708d5d8b34c44389a81c1adf17be4a1b5faf676c
bao <chibaosen@gmail.com>
2018-03-24 14:53:24 +0100

    Made set new password activity screen.

--- app/src/main/java/baosen/codelock/MainActivity.java
+++ app/src/main/java/baosen/codelock/MainActivity.java
@@ -3,6 +3,8 @@ package baosen.codelock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.TelecomManager;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     TelecomManager telecomManager;
@@ -12,15 +14,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //displaySettings();
-        displayPinlock();
+        //displayPinlock();
         //displayPasswordLock();
+        askUserToSetNewPassword();
 
         //EditText passwordEditText = findViewById(R.id.passwordEditText);
-        telecomManager = getTelecomService();
+        //telecomManager = getTelecomService();
     }
 
     private TelecomManager getTelecomService() {
         return (TelecomManager) getSystemService(TELECOM_SERVICE);
+    }
+
+    private void askUserToSetNewPincode() {
+        setTitle("Set new pincode");
+        setContentView(R.layout.activity_set_pinlock);
+        final Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySettings();
+            }
+        });
+    }
+
+    private void askUserToSetNewPassword() {
+        setTitle("Set new password");
+        setContentView(R.layout.activity_set_passwordlock);
+        final Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySettings();
+            }
+        });
     }
 
     private void displaySettings() {
